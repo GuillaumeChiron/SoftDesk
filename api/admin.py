b952fe99199370e24bf6d29a6ff4d687a330b124
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-06-05 16:31:47 +0200

    modifs interface admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from api.models import Contributor, Project, Issue, Comment
 
-# Register your models here.
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("user", "project")
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "author")
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("title", "tag", "priority", "status", "project", "author", "assign")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "issue")
+
+
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Comment, CommentAdmin)
