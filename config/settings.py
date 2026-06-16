b455d735297811e803adddd704b5c7cf6c9534fa
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-06-16 12:15:18 +0200

    suppression de l'authentification par defaut dans les settings

--- config/settings.py
+++ config/settings.py
@@ -127,11 +127,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
 }
-
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
