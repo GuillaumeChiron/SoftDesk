1049ab15e6e68dc1f9a482fdb7a62be2ac400449
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-06-16 12:14:22 +0200

    mise en place des permissions

--- api/permissions.py
+++ api/permissions.py
@@ -21,3 +21,21 @@ class IsIssueAuthorOrProjectContributorReadOnly(BasePermission):
             return project.contributors.filter(user=request.user).exists()
 
         return obj.author == request.user or project.author == request.user
+
+
+class IsCommentAuthorOrProjectAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        project = obj.issue.project
+
+        return obj.author == request.user or project.author == request.user
+
+
+class IsProjectAuthorForContributor(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        project = obj.project
+
+        if request.method in SAFE_METHODS:
+            return project.contributors.filter(user=request.user).exist()
+
+        return project.author == request.user
