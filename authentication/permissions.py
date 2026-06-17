39c0a29a4a0c12bde7261e60a899ca68c1cbe8ab
guillaume <guillaumechiron4@gmail.com>
2026-06-17 15:42:07 +0200

    modification permission IsUser

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -4,5 +4,6 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsUser(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in ["DELETE", "PUT", "PATCH"]:
-            return obj == request.user
+        if request.method in SAFE_METHODS:
+            return True
+        return obj == request.user
